556ce5da8300862871ed60a3c53b64e357b711ec
pajid9763 <pajid9763@wrdsb.ca>
2020-10-02 15:31:12 +0000

    finished random number game

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+import java.util.Random;
 /**
- *
- * @author 
+ * random number game
+ * @author Darian
  */
 public class Main {
 
@@ -9,7 +11,33 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+  Scanner input = new Scanner(System.in);
+  Random rand = new Random();
+
+  // get the random number to guess
+  int randInt = rand.nextInt(100) + 1;
+
+  // declare the guess variable
+  int guess;
+
+  // create a loop to keep guessing
+  do {
+    // get the user's guess
+    System.out.println("Please enter your guess between 1 and 100");
+    guess = input.nextInt();
+
+    // check to see how the guess compares to the random integer
+    if (guess == randInt){
+      System.out.println("You are correct! Good job!");
+    } else if (guess > randInt){
+      System.out.println("You are too high, try again!");
+    }else{
+      System.out.println("You are too low, try again!");
+    }
+  }while (guess != randInt);
+
+
+
+
   }
 }
